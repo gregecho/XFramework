7182b22b920a197514b977fef41e54dadbec2fde
greg <greg_echo@hotmail.com>
2016-08-23 17:01:42 +0800

    Remove useless file

--- XFramework.Services.UnitTests/Implementation/ServiceTests.cs
+++ XFramework.Services.UnitTests/Implementation/ServiceTests.cs
@@ -15,30 +15,30 @@ namespace XFramework.Services.Implementation.UnitTests
     [TestClass()]
     public class ServiceTests
     {
-        private Mock<IPmsOrderRepository> _mockRepository;
-        private IService<PmsOrder> _service;
+        private Mock<IUserRepository> _mockRepository;
+        private IService<User> _service;
         Mock<IDbContextScopeFactory> _mockDbContextScopeFactory;
         Mock<IDbContextScope> _mockDbContextScope;
-        List<PmsOrder> listPmsOrder;
+        List<User> listUser;
 
         [TestInitialize]
         public void Initialize()
         {
             // Mock
-            _mockRepository = new Mock<IPmsOrderRepository>();
+            _mockRepository = new Mock<IUserRepository>();
             _mockDbContextScopeFactory = new Mock<IDbContextScopeFactory>();
             _mockDbContextScope = new Mock<IDbContextScope>();
 
             _mockDbContextScopeFactory.Setup(x => x.Create(It.IsAny<DbContextScopeOption>()))
                 .Returns<DbContextScopeOption>((option) => _mockDbContextScope.Object);
 
-            _service = new PmsOrderService(_mockDbContextScopeFactory.Object, _mockRepository.Object);
+            _service = new UserService(_mockDbContextScopeFactory.Object, _mockRepository.Object);
 
             // Should not test specify object in memory
-            listPmsOrder = new List<PmsOrder>() { 
-             new PmsOrder() { Id = 1, OrderName = "US" }, 
-             new PmsOrder() { Id = 2, OrderName = "India" }, 
-             new PmsOrder() { Id = 3, OrderName = "Russia" } 
+            listUser = new List<User>() { 
+             new User() { ID = 1, Name = "US" }, 
+             new User() { ID = 2, Name = "India" }, 
+             new User() { ID = 3, Name = "Russia" } 
             };
         }
 
@@ -46,9 +46,9 @@ namespace XFramework.Services.Implementation.UnitTests
         public void Service_Query_Test()
         {
             //Arrange 
-            _mockRepository.Setup(x => x.Query(null)).Returns(listPmsOrder.AsQueryable());
+            _mockRepository.Setup(x => x.Query(null)).Returns(listUser.AsQueryable());
             //Act 
-            List<PmsOrder> results = _service.Query().ToList();
+            List<User> results = _service.Query().ToList();
 
             //Assert 
             Assert.IsNotNull(results);
@@ -60,109 +60,109 @@ namespace XFramework.Services.Implementation.UnitTests
         public void Service_Query_WithPredict_Test()
         {
             //Arrange 
-            _mockRepository.Setup(x => x.Query(It.IsAny<Expression<Func<PmsOrder, bool>>>()))
-                  .Returns<Expression<Func<PmsOrder, bool>>, Expression<Func<PmsOrder, object>>[]>((predict, pathes) =>
-                    listPmsOrder.Where(predict.Compile()).AsQueryable()
+            _mockRepository.Setup(x => x.Query(It.IsAny<Expression<Func<User, bool>>>()))
+                  .Returns<Expression<Func<User, bool>>, Expression<Func<User, object>>[]>((predict, pathes) =>
+                    listUser.Where(predict.Compile()).AsQueryable()
                 );
-            //.Callback<Expression<Func<PmsOrder, bool>>, Expression<Func<PmsOrder, object>>[]>((predict, pathes) =>
+            //.Callback<Expression<Func<User, bool>>, Expression<Func<User, object>>[]>((predict, pathes) =>
             // {
-            //     listPmsOrder.Where(predict.Compile()).AsQueryable();
+            //     listUser.Where(predict.Compile()).AsQueryable();
             // });
 
             //Act 
-            List<PmsOrder> results = _service.Query(x => x.OrderName == "US").ToList();
+            List<User> results = _service.Query(x => x.Name == "US").ToList();
 
             //Assert 
             Assert.IsNotNull(results);
             Assert.AreEqual(1, results.Count);
 
             //Verify
-            _mockRepository.Verify(x => x.Query(It.IsAny<Expression<Func<PmsOrder, bool>>>()
-                , It.IsAny<Expression<Func<PmsOrder, object>>[]>()), Times.Once);
+            _mockRepository.Verify(x => x.Query(It.IsAny<Expression<Func<User, bool>>>()
+                , It.IsAny<Expression<Func<User, object>>[]>()), Times.Once);
         }
 
         [TestMethod]
         public void Service_Add_One_Test()
         {
             // Stub
-            _mockRepository.Setup(x => x.Add(It.IsAny<PmsOrder>()))
-                .Returns<PmsOrder>((entity) =>
+            _mockRepository.Setup(x => x.Add(It.IsAny<User>()))
+                .Returns<User>((entity) =>
                      entity
                     )
-                .Callback<PmsOrder>((entity) =>
+                .Callback<User>((entity) =>
                   {
-                      listPmsOrder.Add(entity);
+                      listUser.Add(entity);
                   });
 
             //Act 
-            var result = _service.Add(new PmsOrder { Id = 10, OrderName = "China" });
+            var result = _service.Add(new User { ID = 10, Name = "China" });
             //Assert 
             Assert.IsNotNull(result);
-            Assert.AreEqual(4, listPmsOrder.Count);
+            Assert.AreEqual(4, listUser.Count);
             //Verify
-            _mockRepository.Verify(x => x.Add(It.IsAny<PmsOrder>()), Times.Once);
+            _mockRepository.Verify(x => x.Add(It.IsAny<User>()), Times.Once);
         }
 
         [TestMethod]
         public void Service_Add_Many_Test()
         {
-            _mockRepository.Setup(x => x.Add(It.IsAny<IQueryable<PmsOrder>>()))
-               .Callback<IQueryable<PmsOrder>>((entity) =>
+            _mockRepository.Setup(x => x.Add(It.IsAny<IQueryable<User>>()))
+               .Callback<IQueryable<User>>((entity) =>
                {
                    foreach (var item in entity)
                    {
-                       listPmsOrder.Add(item);
+                       listUser.Add(item);
                    }
                });
 
-            var listPmsOrderInsert = new List<PmsOrder>() { 
-             new PmsOrder() { Id = 11, OrderName = "US1" }, 
-             new PmsOrder() { Id = 12, OrderName = "India1" }, 
-             new PmsOrder() { Id = 13, OrderName = "Russia1" } 
+            var listUserInsert = new List<User>() { 
+             new User() { ID = 11, Name = "US1" }, 
+             new User() { ID = 12, Name = "India1" }, 
+             new User() { ID = 13, Name = "Russia1" } 
             };
 
             //Act 
-            _service.Add(listPmsOrderInsert.AsQueryable());
+            _service.Add(listUserInsert.AsQueryable());
             //Assert 
-            Assert.AreEqual(6, listPmsOrder.Count);
+            Assert.AreEqual(6, listUser.Count);
             //Verify???
-            _mockRepository.Verify(x => x.Add(It.IsAny<IQueryable<PmsOrder>>()), Times.Once);
+            _mockRepository.Verify(x => x.Add(It.IsAny<IQueryable<User>>()), Times.Once);
             _mockDbContextScope.Verify(x => x.SaveChanges(), Times.Once);
         }
 
         [TestMethod()]
         public void Service_Delete_Entity_Test()
         {
-            _mockRepository.Setup(x => x.Delete(It.IsAny<PmsOrder>()))
-              .Callback<PmsOrder>((entity) =>
+            _mockRepository.Setup(x => x.Delete(It.IsAny<User>()))
+              .Callback<User>((entity) =>
               {
-                  listPmsOrder.Remove(entity);
+                  listUser.Remove(entity);
               });
 
             //Act 
-            _service.Delete(listPmsOrder.FirstOrDefault());
+            _service.Delete(listUser.FirstOrDefault());
             //Assert 
-            Assert.AreEqual(2, listPmsOrder.Count);
+            Assert.AreEqual(2, listUser.Count);
             //Verify
-            _mockRepository.Verify(x => x.Delete(It.IsAny<PmsOrder>()), Times.Once);
+            _mockRepository.Verify(x => x.Delete(It.IsAny<User>()), Times.Once);
             _mockDbContextScope.Verify(x => x.SaveChanges(), Times.Once);
         }
 
         [TestMethod()]
         public void Service_Delete_Not_Exist_Entity_Test()
         {
-            _mockRepository.Setup(x => x.Delete(It.IsAny<PmsOrder>()))
-              .Callback<PmsOrder>((entity) =>
+            _mockRepository.Setup(x => x.Delete(It.IsAny<User>()))
+              .Callback<User>((entity) =>
               {
-                  listPmsOrder.Remove(entity);
+                  listUser.Remove(entity);
               });
 
             //Act 
-            _service.Delete(new PmsOrder());
+            _service.Delete(new User());
             //Assert 
-            Assert.AreEqual(3, listPmsOrder.Count);
+            Assert.AreEqual(3, listUser.Count);
             //Verify
-            _mockRepository.Verify(x => x.Delete(It.IsAny<PmsOrder>()), Times.Once);
+            _mockRepository.Verify(x => x.Delete(It.IsAny<User>()), Times.Once);
             _mockDbContextScope.Verify(x => x.SaveChanges(), Times.Once);
         }
 
@@ -170,37 +170,37 @@ namespace XFramework.Services.Implementation.UnitTests
         [ExpectedException(typeof(ArgumentNullException), "entity")]
         public void Service_Delete_Null_Entity_Test()
         {
-            PmsOrder order = null;
+            User order = null;
             _mockRepository.Setup(x => x.Delete(order))
               .Throws<ArgumentNullException>();
 
             //Act 
             _service.Delete(order);
             //Assert 
-            Assert.AreEqual(3, listPmsOrder.Count);
+            Assert.AreEqual(3, listUser.Count);
             //Verify
-            _mockRepository.Verify(x => x.Delete(It.IsAny<PmsOrder>()), Times.Once);
+            _mockRepository.Verify(x => x.Delete(It.IsAny<User>()), Times.Once);
             _mockDbContextScope.Verify(x => x.SaveChanges(), Times.Never);
         }
 
         [TestMethod]
         public void Service_Delete_Many_Entity_Test()
         {
-            _mockRepository.Setup(x => x.Delete(It.IsAny<IQueryable<PmsOrder>>()))
-             .Callback<IQueryable<PmsOrder>>((entity) =>
+            _mockRepository.Setup(x => x.Delete(It.IsAny<IQueryable<User>>()))
+             .Callback<IQueryable<User>>((entity) =>
              {
                  for (int i = 0; i < entity.Count(); i++)
                  {
-                     listPmsOrder.Remove(entity.ElementAt(i));
+                     listUser.Remove(entity.ElementAt(i));
                  }
              });
-            var deltedPmsOrderList = listPmsOrder.Where(x => x.Id > 1);
+            var deltedUserList = listUser.Where(x => x.ID > 1);
             //Act 
-            _service.Delete(deltedPmsOrderList.AsQueryable());
+            _service.Delete(deltedUserList.AsQueryable());
             //Assert 
-            //Assert.AreEqual(1, listPmsOrder.Count);
+            //Assert.AreEqual(1, listUser.Count);
             //Verify
-            _mockRepository.Verify(x => x.Delete(It.IsAny<IQueryable<PmsOrder>>()), Times.Once);
+            _mockRepository.Verify(x => x.Delete(It.IsAny<IQueryable<User>>()), Times.Once);
             _mockDbContextScope.Verify(x => x.SaveChanges(), Times.Once);
         }
     }
